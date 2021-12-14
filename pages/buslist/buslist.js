1d22748cfcdd6e33ebe06be142a5733f614452cb
SmartPolarBear <clevercoolbear@outlook.com>
2021-12-14 20:35:40 +0800

    feat(buslist): load data from the cloud

--- pages/buslist/buslist.js
+++ pages/buslist/buslist.js
@@ -1,80 +1,32 @@
 // pages/buslist/buslist.js
 var util = require('../../utils/util');
-const db = getApp().globalData.db;
+const app = getApp();
+const db = app.globalData.db;
 
 Page({
     /**
      * 页面的初始数据
      */
     data: {
-        "buses": [{
-                "id": 1,
-                "image": "../images/bus_placeholder.png",
-                "bus_plate_number": "闽D11123",
-                "available": 45,
-                "occupied": 3,
-          
-                "datetime": util.formatTime(new Date)
-            },
-            {
-                "id": 2,
-                "image": "../images/bus_placeholder.png",
-                "bus_plate_number": "闽D11323",
-                "available": 42,
-                "occupied": 6,
-               
-                "datetime": util.formatTime(new Date)
-            },
-            {
-                "id": 3,
-                "image": "../images/bus_placeholder.png",
-                "bus_plate_number": "闽D13423",
-                "available": 41,
-                "occupied": 7,
-            
-                "datetime": util.formatTime(new Date)
-            }
-        ]
+        "buses": null
     },
 
     fetchOnlineContent: function () {
-        const bus_seat = db.collection('buses').where({"bus":1});
-        bus_seat.count().then(
-            res=> {
-                console.log(res);
-                this.data=res;
-                // for (let i = 0; i < row_count; i++) {
-                //     let availablity = [];
-                //     for (let j = 0; j < 4; j++) {
-                //         let pushed = false;
-                //         for (let k = 0; k < res.data.length; k++) {
-                //             if (res.data[k].seat.x == i && res.data[k].seat.y == j) {
-                //                 availablity.push(1);
-                //                 console.log(i);
-                //                 console.log(j);
-                //                 pushed = true;
-                //                 break;
-                //             }
-                //         }
-                //         if (!pushed) {
-                //             availablity.push(0);
-                //         }
-                //     }
-                //     rows.push({
-                //         row: i,
-                //         availablity: availablity
-                //     });
-                // }
-
-                // that.setData({
-                //     bus_rows: rows
-                // })
-            }
-        )
+        let that = this
+        const buses = db.collection('bus')
+            .get({
+                success: function (res) {
+                    console.log(res.data)
+                   
+                    that.setData({
+                        "buses": res.data
+                    })
+                }
+            })
     },
 
     onBusItemClick: function (e) {
-        let index=parseInt(e.currentTarget)
+        let index = parseInt(e.currentTarget)
         let item = undefined;
         for (let i = 0; i < this.data.buses.length; i++) {
             if (this.data.buses[i].id == parseInt(e.currentTarget.id)) {
@@ -93,7 +45,7 @@ Page({
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-        // this.fetchOnlineContent();
+        this.fetchOnlineContent();
     },
 
     /**
